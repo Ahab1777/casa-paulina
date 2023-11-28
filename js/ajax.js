a29e9ad82f5083cc78c1ef8e5676c9148e513ace
Leonardo de Pinho <pinholeonardo1777@gmail.com>
2023-11-28 11:21:14 -0300

    links finalizados

---
+++ js/ajax.js
@@ -0,0 +1,19 @@
+// Dynamic Page Load
+
+$(document).ready(function(){
+    const trigger = $('#nav ul li .shortcut'),
+    container = $('#content');
+
+
+    trigger.on('click', function(){
+    
+        const $this = $(this),
+        target = $this.data('target');
+
+        container.load(target + '.html');
+
+        return false;
+    });
+});
+
+
--- js/app.js
+++ js/app.js
@@ -23,11 +23,13 @@ buttons.forEach(button => {
         slides.children[newIndex].dataset.active = true
         delete activeSlide.dataset.active
     })
-})
-
-
-// clima
+}) 
+// weather
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+
+
+
 
 
